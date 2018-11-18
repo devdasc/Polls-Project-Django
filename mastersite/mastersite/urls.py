1af11635c90f35871c3617564d532db16162c43f
devdasc <devdasc777@gmail.com>
2018-11-18 01:48:27 -0500

    -dn

--- mastersite/mastersite/urls.py
+++ mastersite/mastersite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path(r'admin/', admin.site.urls),
-    path(r'polls/', include('polls.urls')),
+    # path(r'polls/', include('polls.urls')),
+    path(r'polls/', include('polls.urls', namespace="polls")),
 ]
